46a2733b0b51c7a581a28a342ac36048ef057a78
DilshanGDT <tharakadilshan506@gmail.com>
2026-04-13 22:53:49 +0530

    Header

--- portfolio-v2/app/layout.js
+++ portfolio-v2/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={`${robotoFont.className} ${outfitFont.className} ${ovoFont.className} h-full antialiased`}
+      className={`${robotoFont.className} ${outfitFont.className} ${ovoFont.className} h-full antialiased leading-8 overflow-x-hidden`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
---
+++ portfolio-v2/components/Header.jsx
@@ -0,0 +1,32 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4'>
+      <div>
+        <Image src={assets.profile_img} alt='' className='rounded-full w-32' />
+      </div>
+      <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-outfit    '>Hi! I'm Tharaka Dilshan 
+            <Image src={assets.hand_icon} alt='' className='w-5' />
+      </h3>
+      <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-roboto'>Full Stack Web Developer in Sri Lanka</h1>
+      <p className='max-w-2xl mx-auto font-outfit'>
+        I create responsive and user-friendly websites and applications using modern technologies.
+      </p>
+
+      <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
+        <a href='#contact' className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2'>Contact Me
+          <Image className='w-4' src={assets.right_arrow_white} alt='Contact' />
+        </a>
+
+        <a href='/sample-resume' download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2'>My Resume
+          <Image className='w-4' src={assets.download_icon} alt='Contact' />
+        </a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
